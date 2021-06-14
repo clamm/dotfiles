daa83b9aa9c82b80fa87da954f82b5213cf67052
clamm <clamm@email.com>
2021-06-14 22:43:31 +0200

    Do what pyenv tells me to do

--- zsh/my-settings.zsh
+++ zsh/my-settings.zsh
@@ -26,6 +26,10 @@ function setjdk() {
 
 
 # pyenv
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init --path)"
+
 # To enable shims and autocompletion add to your profile:
 if which pyenv > /dev/null; then eval "$(pyenv init -)"; fi
 
